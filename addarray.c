294c53579c3ed9e51cc8984c33b0a58c94ffabdc
Prachi <101463081+prachigitam@users.noreply.github.com>
2022-04-13 04:24:52 +0000

    added a new file

---
+++ addarray.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int arr[100]=0;
+  int i,x,p,n=10;
+  for(i=0;i<10;i++)
+    arr[i]=i+1;
+  for(i=0;i<n;i++)
+    printf('%d',arr[i]);
+  printf("\n");
+  x=20;
+  n++;
+  for(i-n-1;i<n;i++)
+    arr[i]=arr[i-1];
+  arr[p-1]=x;
+  for(i=0;i<n;i++)
+    printf("%d",arr[i]);
+  printf("/n");
+  }
