75b00fcce08854ddaf6fa340c1589bc0995592bc
Prachi <101463081+prachigitam@users.noreply.github.com>
2022-05-30 04:49:23 +0000

    traversal

---
+++ preorder.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<malloc.h>
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* createNode(int data){
+    struct node *n; // creating a node pointer
+    n = (struct node *) malloc(sizeof(struct node)); // Allocating memory in the heap
+    n->data = data; // Setting the data
+    n->left = NULL; // Setting the left and right children to NULL
+    n->right = NULL; // Setting the left and right children to NULL
+    return n; // Finally returning the created node
+}
+
+void preOrder(struct  node* root){
+    if(root!=NULL){
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
