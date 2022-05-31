0954c9a5dca919aa7dcd906904c038187e871120
Prachi <101463081+prachigitam@users.noreply.github.com>
2022-05-31 17:43:21 +0000

    modifying output

--- modifyingarray.c
+++ modifyingarray.c
@@ -1,33 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Print on console the array of int
+ */
+void print(int* array, int length);
+void change(int *array,int length);
+
+
+void test_change();
+
+
 int main(){
-    int i, NUll, length = 10;
-    int array[length];
 
-    for (i = 0 ; i < length ; i++)
-        array[i] = i * 10;
-    printf("Before:");
-    print(array, length);
-    change(array, length);
-    printf("After:");
-    print(array, length);
-
-    return 0;
+  test_change();  printf("\n");
+  
+  return 0;
 }
 
-
-void print(int *array,int length)
+void print(int* array, int length)
 {
-    int i;
-    for(i = 0 ; i < length ; i++)
-        printf("%d ", array[i]);
-    printf("\n");
+  int i;
+  for(i = 0 ; i < length ; i++)
+    printf("%d ", array[i]);
+  printf("\n");
 }
 
-
-void change(int **array,int length)
+void change(int *array,int length)
 {
-    *array=malloc(length * sizeof(int) );
-  if(*array == NUll)
-    return;
-  for(int i=0;i<length; i++)
-    (*array)[i]=1;
+  printf("array address inside function: %p\n", array);
+  int i;
+  for(i = 0 ; i < length ; i++)
+    array[i] = 5;
 }
+
+
+void test_change()
+{
+  printf("TEST: change\n");
+  int i, length = 3;
+  int test[3] = {1,2,3};
+
+  printf("Before:");
+  print(test, length);
+  printf("before change, test address: %p\n", test);
+  change(test, 3);
+  printf("After:");
+  print(test, length);
+  printf("after change, test address: %p\n", test);
+}
+/*
+TEST: change
+Before:1 2 3 
+before change, test address: 0x7fffe97bf27c
+array address inside function: 0x7fffe97bf27c
+After:5 5 5 
+after change, test address: 0x7fffe97bf27c*/
+
